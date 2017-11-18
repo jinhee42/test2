c431636d90165ddd413b4f48b8c80af709c31a98
hwan141592 <aitwillz@gmail.com>
2017-11-18 16:46:04 +0900

    테스트생성
    생성

---
+++ src/test2/Idpw.java
@@ -0,0 +1,16 @@
+package test2;
+
+public class Idpw {
+
+	public static void main(String[] args) {
+		String id = "testid";
+		String passwd = "1234";
+		if(id=="testid") {
+			if(passwd=="1234") {
+				System.out.println("로그인 성공입니다!");
+			}
+		}
+	}
+}
+
+
